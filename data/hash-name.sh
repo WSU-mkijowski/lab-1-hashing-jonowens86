7e38c0856347a12499db6397c87e069fb23a197d
Jonathan Owens <owens.240@wright.edu>
2025-09-04 00:53:53 +0000

    made temporary files variables.

--- data/hash-name.sh
+++ data/hash-name.sh
@@ -3,9 +3,23 @@
 INPUT_FILE="quiz_data.csv"
 OUTPUT_FILE="salted-data.csv"
 
-# Check input file exists
+# Check if input file exists
+
+
+
 if [[ ! -f "$INPUT_FILE" ]]; then
   echo "Error: $INPUT_FILE not found!"
   exit 1
 fi
+
+# Make temp files
+
+TMP_SALTS="salts.tmp"
+TMP_HASHES="hashes.tmp"
+
+
+# Step 1: Extract unique user names from the first column
+
+
+awk -F, '{print $1}' "$INPUT_FILE" | sort | uniq > users.tmp
 
